882058876c6df90aca1692626dd690c078748ace
lostedmemarbles <ashleyemarble@gmail.com>
2025-03-21 21:40:54 -0500

    begun individual item scrape

--- mainScraper.py
+++ mainScraper.py
@@ -3,11 +3,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import re
 import os
+import json
 
 nikkiInfoBaseURL = 'https://ln.nikkis.info'
 mainTypes = ['hair', 'dress', 'coat', 'top', 'bottom', 'hosiery', 'shoes', 'makeup', 'accessory/hair-ornament', 'accessory/earrings', 'accessory/scarf', 'accessory/necklace', 'accessory/right-hand-ornament', 'accessory/left-hand-ornament', 'accessory/gloves', 'accessory/right-handheld', 'accessory/left-handheld', 'accessory/both-handheld', 'accessory/waist-decoration', 'accessory/veil', 'accessory/face-accessory', 'accessory/brooch', 'accessory/tattoo', 'accessory/wing', 'accessory/tail', 'accessory/foreground', 'accessory/background', 'accessory/hairpin', 'accessory/ear', 'accessory/head-ornament', 'accessory/ground', 'accessory/skin', 'soul', 'suit'];
 
-itemTypeToLinks = {'H': 'hair'}
 itemIds = []
 
 
@@ -17,10 +17,21 @@ def saveIdFile(typeName, itemIdList):
 
 def itemIdFileExists(typeName):
     return os.path.isfile('./itemIds/{}.id'.format(typeName))
+def itemJSONFileExists(typeName):
+    return os.path.isfile('./itemJSONs/{}.json'.format(typeName))
 
-def getURLFromItemId(itemId):
-    return 'https://ln.nikkis.info/wardrobe/{}/{}'.format(itemTypeToLinks[re.search('[A-Z]+', itemId).group()], re.search('[0-9]+', itemId).group())
+def scrapeItemPage(typeName, baseSite, jsonLoc):
+    itemJSON = {}
+    if itemJSONFileExists(typeName):
+        with open(jsonLoc, 'r') as g:
+            itemJSON = json.loads(g.read())
+    print()
 
+def scrapeItemPages():
+    for mainType in mainTypes:
+        baseSite = '{}/wardrobe/{}'.format(nikkiInfoBaseURL, mainType)
+        jsonFileLocation = './itemJSONs/{}.json'.format(mainType)
+        scrapeItemPage(mainType, baseSite, jsonFileLocation)
 
 def getItemIdFiles():
     driver = webdriver.Chrome()
@@ -29,7 +40,7 @@ def getItemIdFiles():
         if itemIdFileExists(mainType):
             continue
         itemIds = []
-        driver.get('https://ln.nikkis.info/wardrobe/{}'.format(mainType))
+        driver.get('{}/wardrobe/{}'.format(nikkiInfoBaseURL, mainType))
         itemDiv = driver.find_element(By.CLASS_NAME, 'collection.row')
         itemLinkEls = itemDiv.find_elements(By.TAG_NAME, 'a')
         print(len(itemLinkEls))
