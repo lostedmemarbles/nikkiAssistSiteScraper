ef82dc7d49232ed54890c67b1c97ba2246a190fc
lostedmemarbles <ashleyemarble@gmail.com>
2025-03-21 21:25:32 -0500

    added itemId files

--- mainScraper.py
+++ mainScraper.py
@@ -1,25 +1,47 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import re
+import os
 
 nikkiInfoBaseURL = 'https://ln.nikkis.info'
+mainTypes = ['hair', 'dress', 'coat', 'top', 'bottom', 'hosiery', 'shoes', 'makeup', 'accessory/hair-ornament', 'accessory/earrings', 'accessory/scarf', 'accessory/necklace', 'accessory/right-hand-ornament', 'accessory/left-hand-ornament', 'accessory/gloves', 'accessory/right-handheld', 'accessory/left-handheld', 'accessory/both-handheld', 'accessory/waist-decoration', 'accessory/veil', 'accessory/face-accessory', 'accessory/brooch', 'accessory/tattoo', 'accessory/wing', 'accessory/tail', 'accessory/foreground', 'accessory/background', 'accessory/hairpin', 'accessory/ear', 'accessory/head-ornament', 'accessory/ground', 'accessory/skin', 'soul', 'suit'];
 
-#wardrobeLinks = ['https://ln.nikkis.info/wardrobe/hair', 'https://ln.nikkis.info/wardrobe/dress', 'https://ln.nikkis.info/wardrobe/coat', 'https://ln.nikkis.info/wardrobe/top', 'https://ln.nikkis.info/wardrobe/bottom', 'https://ln.nikkis.info/wardrobe/hosiery', 'https://ln.nikkis.info/wardrobe/shoes', 'https://ln.nikkis.info/wardrobe/makeup', 'https://ln.nikkis.info/wardrobe/accessory', 'https://ln.nikkis.info/wardrobe/soul', 'https://ln.nikkis.info/wardrobe/suit'];
-wardrobeLinks = ['https://ln.nikkis.info/wardrobe/hair']
-
-
-
+itemTypeToLinks = {'H': 'hair'}
+itemIds = []
 
 
+def saveIdFile(typeName, itemIdList):
+    with open('./itemIds/{}.id'.format(typeName), 'w') as f:
+        f.write(itemIdList)
 
-def scrapeIt():
+def itemIdFileExists(typeName):
+    return os.path.isfile('./itemIds/{}.id'.format(typeName))
+
+def getURLFromItemId(itemId):
+    return 'https://ln.nikkis.info/wardrobe/{}/{}'.format(itemTypeToLinks[re.search('[A-Z]+', itemId).group()], re.search('[0-9]+', itemId).group())
+
+
+def getItemIdFiles():
     driver = webdriver.Chrome()
     
-    for wardrobeLink in wardrobeLinks:
-        driver.get(wardrobeLink)
+    for mainType in mainTypes:
+        if itemIdFileExists(mainType):
+            continue
+        itemIds = []
+        driver.get('https://ln.nikkis.info/wardrobe/{}'.format(mainType))
+        itemDiv = driver.find_element(By.CLASS_NAME, 'collection.row')
+        itemLinkEls = itemDiv.find_elements(By.TAG_NAME, 'a')
+        print(len(itemLinkEls))
         
+        for itemLinkEl in itemLinkEls:
+            itemId = itemLinkEl.get_attribute('wid')
+            itemIds.append(itemId)
+        print()
+        print(itemIds)
+        print()
+        saveIdFile(mainType, str(itemIds))
+    driver.quit()
         
-        
-        
-        driver.quit()
     
-    
-scrapeIt()
+getItemIdFiles()
